8f81b13c626411d8051b32a28f2f53e64216407d
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-19 14:40:28 -0400

    array notes + constants

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,15 +3,16 @@ import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        int[] numbers = new int[5]; // first way to set up arrays
         numbers[0] = 1;
         numbers[1] = 2;
         System.out.println(Arrays.toString(numbers));
 
-        int[] numbers2 = {2, 4, 5, 8, 6};
+        int[] numbers2 = {2, 4, 5, 8, 6}; // second way to set up arrays
         System.out.println(numbers2.length);
 
         System.out.println(Arrays.toString(numbers2));
+        // CONSTANTS: Add "final" at the beginning so that the value of a variable stays the same.
     }
     }
 
