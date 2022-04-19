3853faeb5bed5ad49fa459cd11537ba4a9ae7c43
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-19 15:17:39 -0400

    PEMDAS

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,15 +1,10 @@
 
 public class main {
     public static void main(String[] args) {
-        // Arithmetic Expressions
-        int add = 10 + 10;
-        int subtract = 10 - 10;
-        int multi = 10 * 10;
-        double divide= (double) 10 / (double) 10;
-        System.out.println(divide); // run this and results will be 1.0
-        //augmented assignment operator
-        int x = 1;
-        x += 2; // this is the same as x = x+2
+        // Order of Operations
+        int x = 10 + 3 * 2; // PEMDAS is used so first parenthesis then multiply or divide then add or subtract
+        System.out.println(x);
+
     }
     }
 
