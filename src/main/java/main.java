ce5ccc79b13b4d16e8166be97bd8ca89a8a46441
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-19 14:25:25 -0400

    both ways to use arrays

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
+import java.util.Date;
+
 public class main {
     public static void main(String[] args) {
-        byte age = 30; // this is better than int because it takes less space + 30 is < 127
-        long viewCount = 3_123_456_789L; // REMEMBER: write _ for , and use an L at the end
-        float price = 10.99F; // just like long, use an F at the end
-        char letter = 'A'; // only for single characters, string is for multiple. use single '' for this.
-        boolean isEligible = false; // either true or false
-        System.out.println(age);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        System.out.println(Arrays.toString(numbers));
+
+        int[] numbers2 = {2, 4, 5, 8, 6};
+        System.out.println(numbers2.length);
+
+        System.out.println(Arrays.toString(numbers2));
     }
-}
+    }
+
